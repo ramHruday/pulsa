1866681ed7509cc888e609c58785758fdd76c516
rhruday <ramahrudayb@gmail.com>
2019-12-15 13:01:29 +0530

    app run added

--- __init__.py
+++ __init__.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
-    )
-    from . import main
-    app.register_blueprint(main.bp)
-
-    return app
+    try:
+        app = Flask(__name__)
+        app.config.from_mapping(
+            SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
+        )
+        from . import main
+        app.register_blueprint(main.bp)
+        print('y')
+        return app.run()
+    except Exception as e:
+        print(e)
