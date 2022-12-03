b855a464610be61e47157cfa66254511fc7dcd7b
Rama Hruday Bandaru <53343166+ramHruday@users.noreply.github.com>
2022-12-03 11:26:39 -0600

    Update Appconfigurations.py

--- common/Appconfigurations.py
+++ common/Appconfigurations.py
@@ -1,23 +1,23 @@
 import base64
 
-KL_PULSE_STATUS_WEB = "https://klpulse.knowledgelens.com/kpulse/templates/addStatus.html"
-KL_PULSE_CREATE_STATUS_API_ENDPOINT = 'https://klpulse.knowledgelens.com/status/createStatus'
+KL_PULSE_STATUS_WEB = "https://kk.knowledgelens.com/"
+KL_PULSE_CREATE_STATUS_API_ENDPOINT = 'https://kk.knowledgelens.com/'
 kl_name = "Rama Hruday Bandaru"
-kl_mail = "rama@knowledgelens.com"
+kl_mail = ""
 
 # bit bucket style
-BIT_BUCKET_API_ENDPOINT = 'https://api.bitbucket.org/2.0/repositories/zs_businesstech/amgen-search/commits'
-bit_bucket_user_cred = base64.b64encode(b"rh21686:zsa!21686h").decode("ascii")
+BIT_BUCKET_API_ENDPOINT = 'https://api.bitbucket.org/2.0/'
+bit_bucket_user_cred = 
 bit_bucket_headers = {'Authorization': 'Basic %s' % bit_bucket_user_cred}
 bit_bucket_status_headers = {
     'Authorization': 'Basic %s' % bit_bucket_user_cred}
-bit_bucket_user = "rama.hruday@zs.com"
+bit_bucket_user = ""
 
 # git lab style
 GIT_LAB_API_ENDPOINT = "https://gitlab.example.com/api/v4/projects/5/repository/commits"
-git_lab_access_token = "git access token"
+git_lab_access_token = ""
 git_lab_status_headers = {'PRIVATE-TOKEN': git_lab_access_token}
-git_lab_user = "rama@knowledgelens.com"
+git_lab_user = ""
 
 time_format_of_received_string = "%Y-%m-%dT%H:%M:%S+00:00"
 
